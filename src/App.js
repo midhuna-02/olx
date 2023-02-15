eab5c81ab2ef1186ad34859410df39ee29c3e55e
midhuna-02 <midhunamuthungali@gmail.com>
2023-02-16 00:53:11 +0530

    create olx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+
 import './App.css';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+        <Route path='/' element={<Home/>} /> 
+        <Route path='/signup' element={<Signup/>}/>
+
+        </Routes>
+       
+      </Router>
+      
     </div>
   );
 }
