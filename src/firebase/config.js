77685b46107b77768e11b12823b02845ab6ebad4
midhuna-02 <midhunamuthungali@gmail.com>
2023-02-17 00:40:23 +0530

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBrQCtvrhI45usfACYjeefE4hHKyIORtZc",
+    authDomain: "fir-8d849.firebaseapp.com",
+    projectId: "fir-8d849",
+    storageBucket: "fir-8d849.appspot.com",
+    messagingSenderId: "181364320618",
+    appId: "1:181364320618:web:4997872b94cd333f351d13",
+    measurementId: "G-J10X13FS1G"
+  };
+ export default firebase.initializeApp(firebaseConfig);
+ 
